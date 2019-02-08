65ac5c8477a118e50a1bb1a4f0be7349de71e5e5
Anthony Li <459016795@qq.com>
2019-02-08 16:20:18 -0600

    Add files via upload

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,15 +9,80 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-
+=begin
   def index
     @movies = Movie.all
   end
+=end
+  def index
 
+    redirect_link = false
+    
+    if params[:ratings].nil? && !session[:ratings].nil? && @checked_values.nil?
+      @checked_values = session[:ratings]
+      redirect_link = true
+    else
+      session.delete(:ratings)
+      session[:ratings] = params[:ratings]
+      @checked_values = session[:ratings]
+      redirect_link = false   
+    end
+    
+    if params[:sort].nil? && !session[:sort].nil? && @sorted_values.nil?
+      @sorted_values = session[:sort]
+      redirect_link = true
+    else
+      session.delete(:sort)
+      session[:sort] = params[:sort]
+      @sorted_values = session[:sort]
+      redirect_link = false      
+    end
+    
+    if redirect_link
+      redirect_to movies_path({:ratings => @checked_values, :sort => @sorted_values})
+    end 
+      
+    @all_ratings = Movie.ratings
+    
+    # If Parameters exist, store them in the session.
+    if !params[:ratings].nil?
+      session.delete(:ratings)
+      session[:ratings] = params[:ratings]
+      checkbox = params[:ratings]
+      @checked_values = params[:ratings]
+    end
+   
+    if !params[:sort].nil?
+      sorting = params[:sort]
+      session.delete(:sort)
+      session[:sort] = sorting
+      @sorted_values = session[:sort]
+    elsif !session[:sort].nil?
+      sorting = session[:sort]
+    end
+    
+    if sorting == 'title'
+      sort_order = :title
+      
+    elsif sorting == 'release_date'
+      sort_order = :release_date
+    else
+      sort_order = ''
+    end
+    
+    if checkbox.respond_to?(:keys)
+        @movies = Movie.where("rating in (:all_ratings)", {all_ratings: checkbox.keys}).order(sort_order)
+    else
+      @movies = Movie.all.order(sort_order)
+      @checked_values = @all_ratings
+    end
+
+  end
   def new
     # default: render 'new' template
   end
 
+  # if create fails, throw exception, by using !
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+    def self.ratings
+
+        distinct_ratings = Array.new
+        Movie.select("rating").order("rating").distinct.each { |rating| distinct_ratings.push(rating.read_attribute("rating")) }
+        distinct_ratings
+    end
 end
